210b79a4d19b16280e67bbb31273cf9ade43b9c0
Grace Silvaroli <99274228+GraceSilvaroli@users.noreply.github.com>
2022-03-20 00:24:54 +0000

    edit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
-// Created by: Grace S
+// Created by: Ms Raffin
 // Created on: Mar 2022
 // This file contains the JS functions for index.html
 
-function doMathClicked () {
-  // this function does basic math
-  document.getElementById('add-math').innerHTML = '7 + 4 = ' + (7 + 4)
-  document.getElementById('subtract-math').innerHTML = '21 - 85 =  ' + (21 - 85)
-  document.getElementById('multiply-math').innerHTML = '3 + 49 * 6 = ' + (3 + 49 * 6)
-  document.getElementById('divide-math').innerHTML = '(81 / 9) + 3 = ' + ((4 / 2) + 3)
-  document.getElementById('exponent-math').innerHTML = '8 + 9 ** 3 = ' + (8 + 9 ** 3)
-	document.getElementById('square-root-math').innerHTML = '√(64) = ' + Math.sqrt(64)
+function calcArea () {
+  // claculates the area for the rectangle
+  document.getElementById('calc-area').innerHTML = 'The area is ' + (5 * 3) + 'cm2'
+}
+function calcPerimeter () {
+  document.getElementbyId('calc-per').innterHTML = 'the Perimeter is ' + (2*5 + 2*3) + 'cm'
 }
