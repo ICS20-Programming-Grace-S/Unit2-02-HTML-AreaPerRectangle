dcd36d0cd0eae0de0c399e59d51d307913e58025
Grace Silvaroli <99274228+GraceSilvaroli@users.noreply.github.com>
2022-03-23 01:34:59 +0000

    Update

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-// Created by: Ms Raffin
+// Created by: Grace S
 // Created on: Mar 2022
 // This file contains the JS functions for index.html
 
 function AreaClicked() {
-  // claculates the area for the rectangle
+  // calculates the area for the rectangle
   document.getElementById('calc-area').innerHTML = 'The area is ' + (12 * 5) + 'cm²'
 }
 
 function PerimeterClicked() {
-  // claculates the area for the rectangle
+  // calculates the perimeter for the rectangle
   document.getElementById('calc-per').innerHTML = 'The Perimeter is ' + (2*12 + 2*5) + 'cm'
 }
